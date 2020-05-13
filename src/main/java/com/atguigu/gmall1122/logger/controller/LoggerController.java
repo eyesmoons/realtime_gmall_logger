a71441dad57b1b1c29eec1d980037028c524a1cf
shengyu <shengyu@eyesmoons.com>
2020-05-13 18:32:57 +0800

    bug fixed

--- src/main/java/com/atguigu/gmall1122/logger/controller/LoggerController.java
+++ src/main/java/com/atguigu/gmall1122/logger/controller/LoggerController.java
@@ -8,32 +8,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Controller  // 返回页面
 @RestController //返回字符串值  ==  @Controller + 方法上 @ResponseBody
 @Slf4j
 public class LoggerController {
 
-        @Autowired
-        KafkaTemplate kafkaTemplate;
+    private final static Logger logger = LoggerFactory.getLogger(LoggerController.class);
 
+    @Autowired
+    KafkaTemplate kafkaTemplate;
 
-        @RequestMapping("/applog")
-        public  String applog(@RequestBody String logString ){
-            log.info(logString);
-            JSONObject jsonObject = JSON.parseObject(logString);
-            if(jsonObject.getString("start")!=null&&jsonObject.getString("start").length()>0){
-                kafkaTemplate.send("GMALL_START",logString);
-            }else{
-                kafkaTemplate.send("GMALL_EVENT",logString);
-            }
-            return  "success";
+    @RequestMapping("/applog")
+    public  String applog(@RequestBody String logString ){
+        logger.info(logString);
+        JSONObject jsonObject = JSON.parseObject(logString);
+        if(jsonObject.getString("start")!=null&&jsonObject.getString("start").length()>0){
+            kafkaTemplate.send("GMALL_START",logString);
+        }else{
+            kafkaTemplate.send("GMALL_EVENT",logString);
         }
-
-
+        return  "success";
+    }
 }
